d2b4c9f7af988847f2746b3823fac41c05b0867c
Neil Brennan <neil.brennan1@ppb.com>
2020-05-31 16:02:44 +0100

    Add User(s) class to to Django Admin

--- users_api/models.py
+++ users_api/models.py
@@ -38,7 +38,6 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=60, unique=True)
-    is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
 
