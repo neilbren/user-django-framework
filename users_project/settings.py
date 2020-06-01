387000b3c08b26d522ff8b0d6b1489a7786d41f3
Neil Brennan <neil.brennan1@ppb.com>
2020-06-01 23:47:18 +0100

    Remove global authentication

--- users_project/settings.py
+++ users_project/settings.py
@@ -47,7 +47,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.permissions.IsAuthenticated',
+#        'rest_framework.permissions.IsAuthenticated',
     ),
 }
 
